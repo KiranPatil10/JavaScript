b43c35e1252b659067f8b7eef5f1deaa2d253d04
KiranPatil10 <141115928+KiranPatil10@users.noreply.github.com>
2025-01-24 18:33:18 +0000

    container config

---
+++ 01_basics/test.js
@@ -0,0 +1,7 @@
+console.log("Kiran")
+// step 1: create a repo
+// step 2: open codespace
+// step 3: go to command pallet and search container and select node + js 
+
+// To save 
+// go to source control and add the changed files with a message and commit and then push 
